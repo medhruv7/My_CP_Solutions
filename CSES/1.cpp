f8ac219824f8a937fc8591a596acba90dca636f7
medhruv7 <medhruv7@gmail.com>
2019-06-21 00:34:34 +0530

    Add files via upload

---
+++ CSES/1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+#define vi vector<int>
+#define vpii vector<pair<int,int>> 
+#define all(V) V.begin(),V.end()
+#define mpii map<int,int> 
+#define mpci map<char,int> 
+#define fi first
+#define si second
+
+const int N = 1e5+10;
+const int mod = 1e9 + 7;
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+}
+
